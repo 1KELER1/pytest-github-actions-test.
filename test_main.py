4a61e46956effb300ee842703086e3f2d7d5bb8c
1KELER1 <123807688+1KELER1@users.noreply.github.com>
2025-03-14 22:08:44 +0800

    изменение теста

--- test_main.py
+++ test_main.py
@@ -4,4 +4,4 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
-    assert add(2, 3) == 10  # Ошибка здесь!
+    assert add(2, 3) == 6  # Ошибка здесь!
